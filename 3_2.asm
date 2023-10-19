b8a6c3eb46e03c0736424ead159b615cfe032b03
PhaseMaze <120268282+Xedim@users.noreply.github.com>
2023-10-19 17:56:59 +0300

    Update 3_2.asm

--- 3_2.asm
+++ 3_2.asm
@@ -6,10 +6,12 @@
 ExitProcess proto, dwExitCode:dword
 
 .data
-	array_1 dword 15, 33, 29, 3, 66
+	array_1 dword 15, 3, 29, 37, 66
 	Count_1 = ($ - array_1) / TYPE array_1
 	array_2 dword 19, 22, 3, 15, 99, 22, 66
 	Count_2 = ($ - array_2) / TYPE array_2
+
+	err_all_elemets_same byte ?
 
 	result dword ?
 .code
@@ -54,19 +56,24 @@ BS3_array2: add esi,4
 	cmp eax, ebx
 	jg Arr_1_greater
 	mov ecx, LENGTHOF array_2
-	jmp Ecx_Ready
+	mov edx, LENGTHOF array_1
+	jmp Greater_Length_Found
 	Arr_1_greater:
 	mov ecx, LENGTHOF array_1
-	Ecx_Ready:
+	mov edx, LENGTHOF array_2
+	Greater_Length_Found:
 	mov edi, OFFSET array_1
 	mov eax, [edi]
 	mov esi, OFFSET array_2
 	mov ebx, [esi] 
 
-Comparing_elements: cmp eax, ebx
+Comparing_elements: cmp edx, 0
+					je All_elements_same
+					cmp eax, ebx
 					jl End_of_comparing
 					jne If_Greater
 					dec ecx
+					dec edx
 					add edi, TYPE array_1
 					add esi, TYPE array_2
 					mov eax, [edi]
@@ -78,6 +85,14 @@ Comparing_elements: cmp eax, ebx
 					loop Comparing_elements
 
 End_of_comparing:	mov result, eax 
+					jmp End_of_code
+
+All_elements_same:  cmp edx, 0
+					sete err_all_elemets_same
+					jmp End_of_code
+
+
+End_of_code:
 
 invoke ExitProcess, 0
 main endp 
